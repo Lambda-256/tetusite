f044bc3da28320d6b9f2038c76d4e2ee94e5857d
Lambda-256 <6ec0meaher0@gmail.com>
2024-07-26 20:42:16 +0900

    ディレクトリ移動

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -5,7 +5,7 @@ export const Header: FC = () => {
   return (
     <header className="px-4 lg:px-6 h-14 flex items-center bg-background">
       <Link
-        href="#"
+        href="/"
         className="flex items-center justify-center"
         prefetch={false}
       >
@@ -14,7 +14,7 @@ export const Header: FC = () => {
       </Link>
       <nav className="ml-auto flex gap-4 sm:gap-6">
         <Link
-          href="#"
+          href={'/menu'}
           className="text-sm font-medium hover:underline underline-offset-4 text-primary-foreground text-black"
           prefetch={false}
         >
