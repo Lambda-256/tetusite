00f308309ef3360f4c9bfd69d56a86e13f7fa18f
Lambda-256 <6ec0meaher0@gmail.com>
2024-07-26 20:48:34 +0900

    １チョケ

--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -5,7 +5,14 @@ export const Footer: FC = () => {
   return (
     <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t bg-muted">
       <p className="text-xs text-muted-foreground">
-        &copy; 2024 FromAgent. All rights reserved.
+        &copy; 2024{' '}
+        <Link
+          href="https://x.com/fromA_JP"
+          className="text-xs hover:underline underline-offset-4 text-muted-foreground"
+        >
+          FromAgent
+        </Link>
+        . All rights reserved.
       </p>
       <nav className="sm:ml-auto flex gap-4 sm:gap-6">
         <Link
