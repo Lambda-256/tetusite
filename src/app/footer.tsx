ee2b576142b89601847c07adf74821c779d8358d
Lambda-256 <6ec0meaher0@gmail.com>
2024-07-26 20:19:29 +0900

    ヘッダーフッターの切り分け

---
+++ src/app/footer.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import { FC } from 'react';
+
+export const Footer: FC = () => {
+  return (
+    <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t bg-muted">
+      <p className="text-xs text-muted-foreground">
+        &copy; 2024 FromAgent. All rights reserved.
+      </p>
+      <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+        <Link
+          href="https://www.instagram.com/hogushi_kaba/?igsh=dDM0MnR2MGtmbzJn&utm_source=qr"
+          className="text-xs hover:underline underline-offset-4 text-muted-foreground"
+          prefetch={false}
+        >
+          Instagram
+        </Link>
+        <Link
+          href="https://line.me/R/ti/p/@261yelbv?ts=06301001&oat_content=url"
+          className="text-xs hover:underline underline-offset-4 text-muted-foreground"
+          prefetch={false}
+        >
+          LINE
+        </Link>
+        <Link
+          href="https://www.ekiten.jp/shop_74259373/"
+          className="text-xs hover:underline underline-offset-4 text-muted-foreground"
+          prefetch={false}
+        >
+          ekiten
+        </Link>
+      </nav>
+    </footer>
+  );
+};
---
+++ src/app/header.tsx
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+import { FC } from 'react';
+
+export const Header: FC = () => {
+  return (
+    <header className="px-4 lg:px-6 h-14 flex items-center bg-background">
+      <Link
+        href="#"
+        className="flex items-center justify-center"
+        prefetch={false}
+      >
+        Icon
+        <span className="sr-only">Acme Inc</span>
+      </Link>
+      <nav className="ml-auto flex gap-4 sm:gap-6">
+        <Link
+          href="#"
+          className="text-sm font-medium hover:underline underline-offset-4 text-primary-foreground text-black"
+          prefetch={false}
+        >
+          施術メニュー
+        </Link>
+        <Link
+          href="#"
+          className="text-sm font-medium hover:underline underline-offset-4 text-primary-foreground text-black"
+          prefetch={false}
+        >
+          予約方法
+        </Link>
+        <Link
+          href="#"
+          className="text-sm font-medium hover:underline underline-offset-4 text-primary-foreground text-black"
+          prefetch={false}
+        >
+          カッサとは
+        </Link>
+
+        <Link
+          href="#"
+          className="text-sm font-medium hover:underline underline-offset-4 text-primary-foreground text-black"
+          prefetch={false}
+        >
+          アクセス
+        </Link>
+      </nav>
+    </header>
+  );
+};
