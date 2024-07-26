dd2b93799719584a1f10dc187cd79365db42ca31
Lambda-256 <6ec0meaher0@gmail.com>
2024-07-26 20:50:01 +0900

    不要importの削除

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,8 +1,5 @@
-import Link from 'next/link';
 import { FC } from 'react';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
-// import { Separator } from '@/components/ui/separator';
-// import { Button } from '@/components/ui/button';
 import { Header } from './ui/header';
 import { Footer } from './ui/footer';
 import { BusinessTime } from './ui/businessTime';
